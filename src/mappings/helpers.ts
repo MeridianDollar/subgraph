6696e59b61624f833ef0c44aa533936e0c1fdc39
TJ <58812386+TJ-2@users.noreply.github.com>
2024-09-04 16:06:04 +0200

    initial commit

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -9,7 +9,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x1F2542D8F784565D526eeaDC9F1ca8Fbb75e5996'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
